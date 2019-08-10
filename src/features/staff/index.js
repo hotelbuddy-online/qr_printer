aac7ca818d62358d20dd20dada43e09db1460a46
hotelbuddy-online <gigglesthethird@gmail.com>
2019-08-10 09:50:16 +0000

    added template

--- src/features/staff/index.js
+++ src/features/staff/index.js
@@ -3,3 +3,5 @@ export { default as QrCodes } from './settings/qrCodes/QrCodes';
 export { default as QrCodesPrinter } from './settings/qrCodes/QrCodesPrinter';
 export { default as TestBreak } from './settings/qrCodes/TestBreak';
 export { default as TemplateDetails } from './settings/qrCodes/TemplateDetails';
+export { default as ScheduledJob } from './settings/scheduler/ScheduledJob';
+export { default as ScheduledJobs } from './settings/scheduler/ScheduledJobs';
